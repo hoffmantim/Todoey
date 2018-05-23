da33b4c2185bce0ab57035a853a312305f48f3f3
hoffmantim <hoffmant@hilbertk12.org>
2018-05-23 14:27:32 -0500

    fixed scrolling bug by creating custom Item class; removed UserDefaults for persisting data due to conflict with custom class object

--- Todoey/Controlers/TodoListViewController.swift
+++ Todoey/Controlers/TodoListViewController.swift
@@ -10,27 +10,35 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray : [String] = [""]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = defaults.array(forKey: "todoListArray") as? [String] {
-            itemArray = items
-        }
+       
+        
+        
+        
     }
 
     //MARK: Tableview Datasource Methods
     
     //TODO: Create the Tableview Cells here:
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.isDone == true ? .checkmark : .none
+        
         return cell
     }
     
@@ -45,14 +53,12 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-       
-        //print(itemArray[indexPath.row])
-        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
+        
+        
+        
+        tableView.reloadData()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -67,9 +73,12 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen when user clicks add item button on UIAlert
-            self.itemArray.append(textField.text!)
             
-            self.defaults.setValue(self.itemArray, forKey: "todoListArray")
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+    
             
             self.tableView.reloadData()
         }
---
+++ Todoey/DataModel/ItemsDataModel.swift
@@ -0,0 +1,17 @@
+//
+//  ItemsDataModel.swift
+//  Todoey
+//
+//  Created by Tim Hoffman on 5/23/18.
+//  Copyright © 2018 Tim Hoffman. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    var isDone : Bool = false
+    
+}
